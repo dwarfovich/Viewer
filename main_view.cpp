bac5a70e4da390fb6d995056e1669feb6acde457
Tim <ouncetim@gmail.com>
2020-12-26 15:58:53 +0300

    minor

--- main_view.cpp
+++ main_view.cpp
@@ -4,23 +4,12 @@
 
 MainView::MainView(QWidget *parent)
     : QGraphicsView{parent}
-    , positive_factor_{1.1}
-    , negative_factor_{1. / positive_factor_}
     , click_location_{}
     , scene_center_{}
     , is_scrolling_{false}
 {
     this->setTransformationAnchor (QGraphicsView::AnchorUnderMouse);
     this->setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
-}
-
-void MainView::wheelEvent(QWheelEvent *event)
-{
-    if (event->angleDelta().y() > 0){
-        scale(positive_factor_, positive_factor_);
-    } else {
-        scale(negative_factor_, negative_factor_);
-    }
 }
 
 void MainView::mousePressEvent(QMouseEvent *event)
--- main_view.hpp
+++ main_view.hpp
@@ -9,14 +9,11 @@ public:
     MainView(QWidget *parent = nullptr);
 
 protected:
-    void wheelEvent (QWheelEvent* event);
     void mousePressEvent   (QMouseEvent *event);
     void mouseMoveEvent    (QMouseEvent *event);
     void mouseReleaseEvent (QMouseEvent *event);
 
 private:
-    const qreal positive_factor_;
-    const qreal negative_factor_;
     QPointF click_location_;
     QPointF scene_center_;
     bool is_scrolling_;
